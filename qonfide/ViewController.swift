b00bcf46facddc9720e98470edf35da4dc4ca358
hnslmp <ancemo14@gmail.com>
2022-06-10 11:26:45 +0700

    Initial Commit

---
+++ qonfide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qonfide
+//
+//  Created by Hansel Matthew on 10/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

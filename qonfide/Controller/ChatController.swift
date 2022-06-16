0ca5f63f520c23379b26b2d1a3bc6533e2cfc669
hnslmp <ancemo14@gmail.com>
2022-06-16 22:26:17 +0700

    feat: static bubble chat + todays date in navigation bar

---
+++ qonfide/Controller/ChatController.swift
@@ -0,0 +1,77 @@
+//
+//  ChatController.swift
+//  qonfide
+//
+//  Created by Hansel Matthew on 16/06/22.
+//
+
+import UIKit
+
+private let reuseIdentifier = "MessageCell"
+
+class ChatController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    init(){
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    
+    // MARK: - Helpers
+    
+    func configureUI(){
+        
+        configureNavigationBar(withTitle: getDate(), preferLargeTitles: false)
+        collectionView.backgroundColor = .white
+        collectionView.register(MessageCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.alwaysBounceVertical = true
+        collectionView.keyboardDismissMode = .interactive
+    }
+    
+    
+    func getDate() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let result = dateFormatter.string(from: date)
+        return result
+    }
+}
+
+extension ChatController{
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MessageCell
+        return cell
+    }
+}
+
+extension ChatController: UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 16, left: 0, bottom: 16, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
+        let estimatedSizeCell = MessageCell(frame: frame)
+        let targetSize = CGSize(width: view.frame.width, height: 10000)
+        let estimatedSize = estimatedSizeCell.systemLayoutSizeFitting(targetSize)
+        return .init(width: view.frame.width, height: estimatedSize.height)
+    }
+}
---
+++ qonfide/View/MessageCell.swift
@@ -0,0 +1,70 @@
+//
+//  MessageCell.swift
+//  qonfide
+//
+//  Created by Hansel Matthew on 16/06/22.
+//
+
+import UIKit
+
+class MessageCell: UICollectionViewCell{
+    
+    // MARK: - Properties
+    var bubbleLeftAnchor: NSLayoutConstraint!
+    var bubbleRightAnchor: NSLayoutConstraint!
+    
+    private let profileImageView: UIImageView = {
+       let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    private let textView: UITextView = {
+        let tv = UITextView()
+        tv.backgroundColor = .clear
+        tv.font = .systemFont(ofSize: 16)
+        tv.isScrollEnabled = false
+        tv.isEditable = false
+        tv.textColor = .white
+        tv.text = "Hey, I'm Bob. I'm here to help you with your emotions. Can you first tell me what is affecting your emotion?"
+        return tv
+    }()
+    
+    private let bubbleContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemCyan
+        return view
+    }()
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect){
+        super.init(frame: frame)
+//        backgroundColor = .red
+        
+//        addSubview(profileImageView)
+//        profileImageView.anchor(left: leftAnchor, bottom: bottomAnchor , paddingLeft: 8, paddingBottom: -4)
+//        profileImageView.setDimensions(height: 32, width: 32)
+//        profileImageView.layer.cornerRadius = 32/2
+        
+        addSubview(bubbleContainer)
+        bubbleContainer.layer.cornerRadius = 12
+        bubbleContainer.anchor(top: topAnchor, bottom: bottomAnchor)
+        bubbleContainer.widthAnchor.constraint(lessThanOrEqualToConstant: 250).isActive = true
+        
+        bubbleLeftAnchor = bubbleContainer.leftAnchor.constraint(equalTo: leftAnchor, constant: 12)
+        bubbleLeftAnchor.isActive = false
+        
+        bubbleRightAnchor = bubbleContainer.rightAnchor.constraint(equalTo: rightAnchor, constant: 12)
+        bubbleRightAnchor.isActive = true
+        
+        bubbleContainer.addSubview(textView)
+        textView.anchor(top:bubbleContainer.topAnchor, left: bubbleContainer.leftAnchor, bottom: bubbleContainer.bottomAnchor, right: bubbleContainer.rightAnchor, paddingTop: 4, paddingLeft: 12, paddingBottom: 4,paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}

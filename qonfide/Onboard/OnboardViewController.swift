c791bcc287363203f7e688f4a7c9561621bf02ed
vitaciminIPI <kennysc721@gmail.com>
2022-06-17 00:15:55 +0700

    Adding Slide animation

--- qonfide/Onboard/OnboardViewController.swift
+++ qonfide/Onboard/OnboardViewController.swift
@@ -26,6 +26,7 @@ class OnboardViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         self.view.layoutIfNeeded()
         
         self.scrollView.delegate = self
@@ -40,38 +41,56 @@ class OnboardViewController: UIViewController, UIScrollViewDelegate {
             let slide = UIView(frame: scrollFrame)
             
 //            subviews (ex: title, desc, etc)
-            let backgroundImg = UIImageView.init(image: UIImage(named: onboardData[index].imageView))
-            backgroundImg.frame = CGRect(x: 0, y: 336, width: 414, height: 560)
+            let backgroundImg = UIImageView.init(image: UIImage(named: onboardData[index].imageBackGround))
+            backgroundImg.frame = CGRect(x: 0, y: 236, width: scrollWidth, height: 560)
             backgroundImg.contentMode = .scaleToFill
-//            backgroundImg.center = CGPoint(x: scrollWidth/2, y: scrollHeight/2 - 50)
 //
             let imageView = UIImageView.init(image: UIImage(named: onboardData[index].imageView))
             imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
-            imageView.contentMode = .scaleToFill
-            imageView.center = CGPoint(x: scrollWidth/2, y: scrollHeight/2 - 50)
+            imageView.contentMode = .scaleAspectFill
+            imageView.center = CGPoint(x: scrollWidth/2, y: scrollHeight/2 - 250)
             
-            let title = UILabel.init(frame: CGRect(x: 32, y: imageView.frame.maxY + 30, width: scrollWidth-63, height: 30))
+            let title = UILabel.init(frame: CGRect(x: 32, y: imageView.frame.maxY + 30, width: scrollWidth - 100, height: 85))
             title.textAlignment = .left
-            title.font = UIFont.boldSystemFont(ofSize: 20.0)
+            title.numberOfLines = 2
+            title.textColor = .white
+            title.font = UIFont.boldSystemFont(ofSize: 34.0)
             title.text = onboardData[index].titles
             
-            let desc = UILabel.init(frame: CGRect(x: 32, y: title.frame.maxY+10, width: scrollWidth-64, height: 30))
+            let desc = UILabel.init(frame: CGRect(x: 32, y: title.frame.maxY+10, width: scrollWidth-64, height: 100))
             desc.textAlignment = .left
             desc.numberOfLines = 3
+            desc.textColor = .white
             desc.font = UIFont.systemFont(ofSize: 18)
             desc.text = onboardData[index].desc
             
-            let button = UIButton(frame: CGRect(x: 10, y: desc.frame.maxY + 10, width: 150, height: 50))
+            let button = UIButton(frame: CGRect(x: 32, y: desc.frame.maxY + 20, width: scrollWidth-70, height: 50))
             button.setTitleColor(.white, for: .normal)
+            button.layer.cornerRadius = 10
             button.backgroundColor = UIColor(red: 53/255, green: 74/255, blue: 166/255, alpha: 100)
             button.setTitle("Get Started", for: .normal)
             button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+            
+            slide.addSubview(backgroundImg)
+            slide.addSubview(imageView)
+            slide.addSubview(title)
+            slide.addSubview(desc)
             
             if index == onboardData.count - 1 {
                 slide.addSubview(button)
             }
             
+            scrollView.addSubview(slide)
         }
+        
+//        set width to accomodate all slides
+        scrollView.contentSize = CGSize(width: scrollWidth * CGFloat(onboardData.count), height: scrollHeight)
+        
+//        disable verti scroll
+        scrollView.contentSize.height = 1.0
+        
+        pageControl.numberOfPages = onboardData.count
+        pageControl.currentPage = 0
         
         // Do any additional setup after loading the view.
     }
@@ -81,6 +100,15 @@ class OnboardViewController: UIViewController, UIScrollViewDelegate {
     }
 
     @IBAction func pageChanged(_ sender: Any) {
+    }
+
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        setIndicatorForCurrentPage()
+    }
+    
+    func setIndicatorForCurrentPage() {
+        let page = (scrollView?.contentOffset.x)! / scrollWidth
+        pageControl?.currentPage = Int(page)
     }
     
     /*

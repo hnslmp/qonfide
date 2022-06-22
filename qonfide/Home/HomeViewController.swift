dc7f5ce06cc0281b8df429dc4f112a6f2f554bb1
vitaciminIPI <kennysc721@gmail.com>
2022-06-22 22:44:38 +0700

    UI done

--- qonfide/Home/HomeViewController.swift
+++ qonfide/Home/HomeViewController.swift
@@ -8,42 +8,50 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var refreshQuoteBtn: UIButton!
+    @IBOutlet weak var quoteTxt: UILabel!
+    @IBOutlet weak var greetTxt: UILabel!
+    @IBOutlet weak var changeMonthBtn: UIButton!
+    @IBOutlet weak var settingBtn: UIButton!
+    @IBOutlet weak var quotesView: UIView!
+    @IBOutlet weak var authorTxt: UILabel!
+    @IBOutlet weak var addEntryBtn: UIButton!
+    
+    lazy private var imageView: UIImageView = {
+        let img = UIImage(named: "Group")
+        let imgView = UIImageView(image: img)
+        imgView.frame = CGRect(x: 45, y: 370, width: 324, height: 350)
+        imgView.contentMode = .scaleAspectFit
+        
+        return imgView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchData { (quotes) in
-            for quote in quotes {
-                print(quote.author)
-            }
-        }
+        viewStyling()
+        tableView.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
     
-    func fetchData(completionHandler: @escaping ([DailyQuotes]) -> Void) {
-        let url = URL(string: "https://type.fit/api/quotes")!
+    func viewStyling() {
+        tableView.isHidden = true
+        view.addSubview(imageView)
+        quotesView.layer.cornerRadius = 10
+        quoteTxt.numberOfLines = 2
+        quoteTxt.text = "\"" + "I wumbo, you wumbo, he she we wumbo" + "\""
+        authorTxt.text = "Patrick Star"
+        authorTxt.font = UIFont.italicSystemFont(ofSize: 17)
         
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            guard let data = data, error == nil else { return }
-            
-            do {
-                let postData = try JSONDecoder().decode([DailyQuotes].self, from: data)
-                completionHandler(postData)
-            } catch {
-                print(error)
-            }
-        }
-        task.resume()
+        changeMonthBtn.setTitle("June 2022 ", for: .normal)
+        changeMonthBtn.setImage(UIImage(systemName: "chevron.down.circle"), for: .normal)
+        changeMonthBtn.contentHorizontalAlignment = .left
+        changeMonthBtn.semanticContentAttribute = .forceRightToLeft
+        
+        addEntryBtn.layer.cornerRadius = 0.5 * addEntryBtn.bounds.size.width
+        addEntryBtn.layer.borderWidth = 1
+        addEntryBtn.layer.borderColor = UIColor.gray.cgColor
+        
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

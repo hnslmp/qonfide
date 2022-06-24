afb9408f5b768c01dc22d36ccd6e6a3919387c39
vitaciminIPI <kennysc721@gmail.com>
2022-06-24 21:26:56 +0700

    Adding settings features

---
+++ qonfide/Setting/Cell/ChangePasswordCell.swift
@@ -0,0 +1,27 @@
+//
+//  ChangePasswordCell.swift
+//  qonfide
+//
+//  Created by daniel stefanus christiawan on 18/06/22.
+//
+
+import UIKit
+
+class ChangePasswordCell: UITableViewCell {
+
+    static let identifier = "ChangePasswordCell"
+    
+    @IBOutlet weak var view: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ qonfide/Setting/UserModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserModel.swift
+//  qonfide
+//
+//  Created by daniel stefanus christiawan on 18/06/22.
+//
+
+import Foundation
+import UIKit
+
+struct Users {
+    var username:String
+    var password:String
+    var email:String
+    var profilePicture:UIImage
+}
